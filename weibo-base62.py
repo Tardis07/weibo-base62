f4cea0c40e6a196ceecb974d91006c3e769991e3
Tardis07 <944730698@qq.com>
2018-10-17 01:54:23 +0800

    fix bug
    fix python3 bug ???

--- weibo-base62.py
+++ weibo-base62.py
@@ -1,13 +1,13 @@
 def div(num):
     d1 = num % 62
-    d2 = num / 62 % 62
-    d3 = num / 62 / 62 % 62
-    d4 = num / 62 / 62 / 62
+    d2 = num // 62 % 62
+    d3 = num // 62 // 62 % 62
+    d4 = num // 62 // 62 // 62
     return [d4,d3,d2,d1]
 
 base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-l = raw_input()
+l = str(input())
 
 if l[2] == '1':
     n = [int(l[:2]),int(l[2:10]),int(l[10:])]
